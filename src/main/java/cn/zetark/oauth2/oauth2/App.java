f912b0e4258f6d8636201520ebd048612c46b617
xiaojihua <240971505@qq.com>
2020-08-01 15:51:08 +0800

    20200801

---
+++ src/main/java/cn/zetark/oauth2/oauth2/App.java
@@ -0,0 +1,13 @@
+package cn.zetark.oauth2.oauth2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
